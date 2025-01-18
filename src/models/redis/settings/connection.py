3203242e1137a1ed244fa0481817213f11b4be6d
NaN <ipfilus@gmail.com>
2025-01-18 15:52:19 -0300

    feat: add models dir and redis connection

---
+++ src/models/redis/settings/connection.py
@@ -0,0 +1,19 @@
+from redis import Redis
+
+
+class RedisConnectionHandler:
+    def __init__(self) -> None:
+        self.__redis_conn = None
+
+    def connect(self) -> Redis:
+        redis_conn = Redis(
+            host="localhost",
+            port=6379,
+            db=0,
+            decode_responses=True,
+        )
+        self.__redis_conn = redis_conn
+        return redis_conn
+    
+    def get_connection(self) -> Redis:
+        return self.__redis_conn
